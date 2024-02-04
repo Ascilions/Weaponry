e87e61d5c26581759682585e417bb70fe63eb1db
Ascilion <freitag.janne@gmail.com>
2024-02-04 14:13:34 +0100

    Initial commit

---
+++ src/client/java/net/pulchrate/Asc_firstmodClient.java
@@ -0,0 +1,10 @@
+package net.pulchrate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Asc_firstmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
